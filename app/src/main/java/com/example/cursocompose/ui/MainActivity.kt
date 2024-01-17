7e01c1d9c9d62abe389f5b1a347eeecfa9e2ad62
m1za3l <antoniopelaezmisael@gmail.com>
2024-01-17 13:05:40 -0600

    [ADD] mover clases a UI

--- app/src/main/java/com/example/cursocompose/ui/MainActivity.kt
+++ app/src/main/java/com/example/cursocompose/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.cursocompose
+package com.example.cursocompose.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -6,7 +6,6 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -29,13 +28,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+
 
 @Preview(showBackground = true)
 @Composable
