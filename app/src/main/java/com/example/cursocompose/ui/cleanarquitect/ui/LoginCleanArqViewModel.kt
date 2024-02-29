5063833da2ab0bd61c5c7c0dcd2db7e094687533
m1za3l <antoniopelaezmisael@gmail.com>
2024-02-29 12:03:14 -0600

    [FIX] me equevoque de viewmodel, lo puse en mvvm en vez de clean Arquitecture

--- app/src/main/java/com/example/cursocompose/ui/cleanarquitect/ui/LoginCleanArqViewModel.kt
+++ app/src/main/java/com/example/cursocompose/ui/cleanarquitect/ui/LoginCleanArqViewModel.kt
@@ -1,11 +1,17 @@
 package com.example.cursocompose.ui.cleanarquitect.ui
 
+import android.util.Log
 import android.util.Patterns
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.cursocompose.ui.cleanarquitect.login.domain.LoginUseCase
+import kotlinx.coroutines.launch
 
 class LoginCleanArqViewModel : ViewModel(){
+
+    val loginUseCase = LoginUseCase()
 
     //solo se puede acceder desde adentro del livedTA
     private val _email = MutableLiveData<String>()
@@ -21,9 +27,20 @@ class LoginCleanArqViewModel : ViewModel(){
         _email.value = email
         _password.value = password
         _isLoginEnable.value = enableLogin(email,password)
+        //para no hacer loginUseCase.doLogin("","")
+        //loginUseCase.("","")
     }
 
     fun enableLogin(email:String, password:String) = Patterns.EMAIL_ADDRESS.matcher(email).matches() && password.length >6
 
-
+    //esta funcion se va llamar cuando el boton de login haya sido pulsado
+    fun onLoginSelected(){
+        viewModelScope.launch{
+            val result = loginUseCase(email.value!!, password.value!!)
+            if(result){
+                //navegar a la siguiente ventana
+                Log.i("m1m1m", "puedes ir: $result")
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/cursocompose/ui/mvvm/login/LoginViewModel.kt
+++ app/src/main/java/com/example/cursocompose/ui/mvvm/login/LoginViewModel.kt
@@ -1,17 +1,11 @@
 package com.example.cursocompose.ui.mvvm.login
 
-import android.util.Log
 import android.util.Patterns
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.example.cursocompose.ui.cleanarquitect.login.domain.LoginUseCase
-import kotlinx.coroutines.launch
 
 class LoginViewModel : ViewModel(){
-
-    val loginUseCase = LoginUseCase()
 
     //solo se puede acceder desde adentro del livedTA
     private val _email = MutableLiveData<String>()
@@ -27,21 +21,9 @@ class LoginViewModel : ViewModel(){
         _email.value = email
         _password.value = password
         _isLoginEnable.value = enableLogin(email,password)
-        //para no hacer loginUseCase.doLogin("","")
-        //loginUseCase.("","")
     }
 
     fun enableLogin(email:String, password:String) = Patterns.EMAIL_ADDRESS.matcher(email).matches() && password.length >6
 
-    //esta funcion se va llamar cuando el boton de login haya sido pulsado
-    fun onLoginSelected(){
-        viewModelScope.launch{
-            val result = loginUseCase(email.value!!, password.value!!)
-            if(result){
-                //navegar a la siguiente ventana
-                Log.i("m1m1m", "puedes ir: $result")
-            }
-        }
-    }
 
 }
