b92e5e9c8ecca6001c0044155f4ce51265cac42b
m1za3l <antoniopelaezmisael@gmail.com>
2024-07-23 14:56:42 -0600

    [ADD] 1-AppTareas, empezzamos configurando los paquetes, data, domain, ui, y preparar dagger hilt

---
+++ app/src/main/java/com/example/cursocompose/todoapp/addtask/ui/TaskScreen.kt
@@ -0,0 +1,8 @@
+package com.example.cursocompose.todoapp.addtask.ui
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TaskScreen(taskViewModel: TaskViewModel) {
+
+}
---
+++ app/src/main/java/com/example/cursocompose/todoapp/addtask/ui/TaskViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.cursocompose.todoapp.addtask.ui
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+
+class TaskViewModel@Inject constructor():ViewModel() {
+}
